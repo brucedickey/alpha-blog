169b73238b68e181829c40a7316cb4a7ed5f5c3b
Bruce Dickey <brucedickey@email.com>
2020-04-02 14:53:29 -0600

    added show action

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_04_02_005209) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
