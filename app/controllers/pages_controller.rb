b2b0449cbffcc9ab53cd6268d5c466b5cd143849
Bruce Dickey <brucedickey@email.com>
2020-04-01 18:57:33 -0600

    added articles table with timestamps and migrations

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+    def home
+        # Empty, so by default by convention expects a 
+        # home.html.erb in app/views/pages/.
+    end
+
+    def about
+    end
+end
