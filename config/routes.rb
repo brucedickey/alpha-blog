c2b45f63bb2fd3eac95db1bbd7cba2784098fdb9
Bruce Dickey <brucedickey@email.com>
2020-04-03 11:22:51 -0600

    added update and delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+
+  # For enabling only some of the REST routes
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  # For enabling all REST routes
+  resources :articles
 end
