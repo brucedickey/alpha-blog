b7018f292bb7d1add3152b8b476e63fb404209dd
Bruce Dickey <brucedickey@email.com>
2020-04-09 14:30:52 -0600

    added action to sign up / create users

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
 
-  # For enabling only some of the REST routes
+  # For enabling only some of the REST routes for articles (list them)
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 
-  # For enabling all REST routes
+  # For enabling all REST routes for articles
   resources :articles
+
+  get 'signup', to: 'users#new'      # Signup form
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
 end
