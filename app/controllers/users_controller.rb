e6e1d7b88bea5e5e1240892c6fbade8046bf8e79
Bruce Dickey <brucedickey@email.com>
2020-04-16 13:08:02 -0600

    now signing in upon successful sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,8 +32,11 @@ class UsersController < ApplicationController
     def create
       @user = User.new(user_params)
       if @user.save
+        # Sign in the user upon sign-up success:
+        session[:user_id] = @user.id
+
         flash[:notice] = "Welcome to the Alpha Blog, #{@user.username}, you have successfully signed up."
-        redirect_to articles_path
+        redirect_to user_path(@user)   # User's index.html.erb page 
       else
         render 'new'
       end
